61ad67074fe8ff3ba0df3d9495135fdb9798d320
MathewsPeter <54246402+MathewsPeter@users.noreply.github.com>
2021-07-25 22:28:17 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -8,24 +8,15 @@
 #include <stdio.h>
 #include <math.h> 
 
-void calcsum(float,float);
-void calcdiff(float,float);
-void calcprod(float,float);
-void calcdiv(float,float);
-void calcpow(float,float);
-
-void calcsum(float a,float b)
-{	printf("%g + %g = %g", a, b, a+b);
-}
-void calcdiff(float a,float b)
-{	printf("%g - %g = %g", a, b, a-b);
-}
-void calcprod(float a,float b)
-{	printf("%g * %g = %g", a, b, a*b);
-}
-void calcdiv(float a,float b)
-{	printf("%g / %g = %g", a, b, a/b);
-}
-void calcpow(float a,float b)
-{	printf("%g ^ %g = %g", a, b, pow(a,b));
+int calcmain(int,int,char); 
+int calcmain(int a, char op, int b)
+{
+ switch(op)
+ {
+  case '+': return (a+b); break;
+  case '-': return (a-b); break;
+  case '*': return (a*b); break;
+  case '/': return (a/b); break;
+  default: printf("Supported operations are + - * / \n");
+  }
 }
